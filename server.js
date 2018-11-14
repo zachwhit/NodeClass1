07b8aeea15fe6dd7f5e7b2c8c38069bb5753d139
zachwhit <zachwhitbread95@gmail.com>
2018-11-14 10:16:10 -0800

    Updated package.json and server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 8080;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -60,7 +62,7 @@ app.get('/404', (request, response) => {
 	});
 });
 
-app.listen(8080, () => {
-	console.log('Server is up on port 8080');
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 });
 

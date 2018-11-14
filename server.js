6ec050b9d3a3d1a840a593951baafedd3b9b20f8
zachwhit <zachwhitbread95@gmail.com>
2018-11-14 09:33:40 -0800

    Added files for week 11 class

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+})
+
+hbs.registerHelper('message', (text) => {
+	return text.toUpperCase();
+})
+
+app.use((request, response, next) => {
+	var time = new Date().toString();
+	//console.log(`${time}: ${request.method} ${request.url}`);
+	var log = `${time}: ${request.method} ${request.url}`;
+	fs.appendFile('server.log', log + '\n', (error) => {
+		if (error) {
+			console.log('Unable to log message');
+		}
+	});
+	next();
+});
+
+// app.use((request, response, next) => {
+//     response.render('maintenance.hbs', {
+//         time: new Date()
+//     });
+// });
+
+app.get('/', (request, response) => {
+	response.send({
+		name: 'Your Name',
+		school: [
+		'BCIT',
+		'SFU',
+		'UBC'
+		]
+	});
+});
+
+app.get('/info', (request, response) => {
+	response.render('about.hbs', {
+		title: 'About page',
+		year: new Date().getFullYear(),
+		welcome: 'Hello'
+	});
+});
+
+app.get('/404', (request, response) => {
+	response.send({
+		error: 'Page not found'
+	});
+});
+
+app.listen(8080, () => {
+	console.log('Server is up on port 8080');
+});
+
